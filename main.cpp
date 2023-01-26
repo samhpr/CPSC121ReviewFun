e15d8e7152e4b3281909c2dec115ff6375cb9d72
Sam Hopper <samuel.hopper23@gmail.com>
2023-01-26 22:56:41 +0000

    init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "vecfuncs.h"
+
+int main() 
+{
+  int min, max;
+  vector<int> userNums;
+  cout << "Hello World!\n";
+
+  fillVector(userNums);
+  cout << "size: " << userNums.size() << endl;
+
+  // task 1: define and call a function
+  // that finds the smallest and largest
+  // values in the vector
+  findExtremes(userNums, min, max);
+  cout << "min: " << min << " max: " << max << endl;
+  // task 2: convert to the 3 file format
+}
